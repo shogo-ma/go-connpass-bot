68175638b9910a5e664fb7fc1d7992c5df1b67d3
shogo-ma <Choroma194@gmail.com>
2017-05-21 01:59:14 +0900

    :+1: add help command

--- main.go
+++ main.go
@@ -1,28 +1,65 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/nlopes/slack"
 )
 
+type SlackBot struct {
+	api *slack.Client
+	rtm *slack.RTM
+}
+
+func NewSlackBot(api *slack.Client) *SlackBot {
+	return &SlackBot{
+		api: api,
+		rtm: api.NewRTM(),
+	}
+}
+
+func (bot *SlackBot) help(channel string) error {
+	contents := "```" + `
+help:
+	help
+show:
+	return resent events from connpass` + "```"
+
+	bot.rtm.SendMessage(
+		bot.rtm.NewOutgoingMessage(
+			contents,
+			channel,
+		),
+	)
+
+	return nil
+}
+
+func (bot *SlackBot) show() error {
+	return nil
+}
+
+func (bot *SlackBot) handleResponse(text, channel string) {
+	if text == "help" {
+		bot.help(channel)
+	} else if text == "show" {
+		bot.rtm.SendMessage(bot.rtm.NewOutgoingMessage(text, channel))
+	}
+}
+
 func run(api *slack.Client) int {
 	api.SetDebug(true)
 
-	rtm := api.NewRTM()
-	go rtm.ManageConnection()
+	slack_bot := NewSlackBot(api)
+	go slack_bot.rtm.ManageConnection()
 
-	for msg := range rtm.IncomingEvents {
-		fmt.Println("Event Received")
+	for msg := range slack_bot.rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
-		case *slack.HelloEvent:
-			log.Println("hello event")
-
 		case *slack.MessageEvent:
-			log.Println("Message: %v\n", ev)
-			rtm.SendMessage(rtm.NewOutgoingMessage("Hello World", ev.Channel))
+			if ev.Type == "message" {
+				slack_bot.handleResponse(ev.Text, ev.Channel)
+			}
 
 		case *slack.InvalidAuthEvent:
 			log.Println("Invalid credentials")
