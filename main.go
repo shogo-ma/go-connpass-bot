76cdb61238c3be19d762687003f867cd1e7c3d64
shogo-ma <Choroma194@gmail.com>
2017-05-21 00:57:01 +0900

    :+1: implement base of bot

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
+func run(api *slack.Client) int {
+	api.SetDebug(true)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		fmt.Println("Event Received")
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			log.Println("hello event")
+
+		case *slack.MessageEvent:
+			log.Println("Message: %v\n", ev)
+			rtm.SendMessage(rtm.NewOutgoingMessage("Hello World", ev.Channel))
+
+		case *slack.InvalidAuthEvent:
+			log.Println("Invalid credentials")
+			return 1
+		}
+	}
+
+	return 1
+}
 
 func main() {
-	fmt.Println("vim-go")
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	os.Exit(run(api))
 }
